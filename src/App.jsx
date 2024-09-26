147cdf665f5cc2dbcb5ab871f90edbe23b1b85e1
Valeria Chubarova <chubarova.valeria99@gmail.com>
2024-09-26 20:46:05 +0300

    improved code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
+import classNames from 'classnames';
 
 export const goods = [
   'Dumplings',
@@ -18,25 +19,27 @@ export const goods = [
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
 
+  const removeSelectedGood = () => {
+    setSelectedGood('');
+  };
+
   return (
     <main className="section container">
-      {selectedGood ? (
-        <h1 className="title is-flex is-align-items-center">
-          {selectedGood} is selected
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-            onClick={() => {
-              setSelectedGood('');
-            }}
-          />
-        </h1>
-      ) : (
-        <h1 className="title is-flex is-align-items-center">
-          No goods selected
-        </h1>
-      )}
+      <h1 className="title is-flex is-align-items-center">
+        {selectedGood ? (
+          <>
+            {selectedGood} is selected
+            <button
+              data-cy="ClearButton"
+              type="button"
+              className="delete ml-3"
+              onClick={() => removeSelectedGood()}
+            />
+          </>
+        ) : (
+          'No goods selected'
+        )}
+      </h1>
 
       <table className="table">
         <tbody>
@@ -44,15 +47,17 @@ export const App = () => {
             <tr
               data-cy="Good"
               key={good}
-              className={
-                good === selectedGood ? 'has-background-success-light' : ''
-              }
+              className={classNames({
+                'has-background-success-light': good === selectedGood,
+              })}
             >
               <td>
                 <button
                   data-cy={selectedGood === good ? 'RemoveButton' : 'AddButton'}
                   type="button"
-                  className={`button ${selectedGood === good ? 'is-info' : ''}`}
+                  className={classNames('button', {
+                    'is-info': selectedGood === good,
+                  })}
                   onClick={() => {
                     setSelectedGood(selectedGood === good ? '' : good);
                   }}
